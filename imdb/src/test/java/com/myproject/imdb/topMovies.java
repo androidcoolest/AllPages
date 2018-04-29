259d6a7968504c925b76276ecc022dc45d8b0f60
SNW <dcoolsam@gmail.com>
2018-04-29 21:51:38 +0200

    first commit

---
+++ imdb/src/test/java/com/myproject/imdb/topMovies.java
@@ -0,0 +1,31 @@
+package com.myproject.imdb;
+import org.testng.annotations.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@Test
+public class topMovies {
+	public void getTopMovies() {
+		
+		//Static Variables
+		String baseUrl="https://www.imdb.com/search/title?groups=top_250&sort=user_rating&my_ratings=exclude";
+		String commonXPath="//*[@id='main']/div/div/div[3]";
+		
+		//Create Selenium Driver
+		System.setProperty("webdriver.chrome.driver","../imdb/src/test/resources/chromedriver");
+		WebDriver driver=new ChromeDriver();
+		
+		//Launch URL
+		driver.get(baseUrl);
+				
+		//Browser to xPath
+		for(int count=1;count<=3;count++)
+		{
+			String linkText=driver.findElement(By.xpath(commonXPath+"/div["+count+"]/div[3]/h3/a")).getText();
+			System.out.println(linkText);
+		}
+		
+		//Close Brower
+		driver.quit();
+	}
+}
